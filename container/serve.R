671f16aeea3f802ee3608f0614463fff938fe208
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-08 11:09:38 +0200

    use littler

--- container/serve.R
+++ container/serve.R
@@ -2,7 +2,5 @@
 Sys.getenv()
 library(plumber)
 
-http_port <- as.numeric(Sys.getenv("AIP_HTTP_PORT"))
-
-pr <- plumber::plumb("api.R")
-pr$run(host='0.0.0.0', port=http_port)
+pr <- plumber::plumb("/root/api.R")
+pr$run(host='0.0.0.0', port=8080)
