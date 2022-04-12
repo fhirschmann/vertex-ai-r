84c1a50fb0495b252949c6032a8916ee21193ad3
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-12 14:43:52 +0200

    simpler serving container

--- container/serve.R
+++ container/serve.R
@@ -2,7 +2,13 @@
 Sys.getenv()
 library(plumber)
 
-system2("gsutil", c("cp", "-r", Sys.getenv("AIP_STORAGE_URI"), "."))
+#system2("gsutil", c("cp", "-r", Sys.getenv("AIP_STORAGE_URI"), "."))
 
-pr <- plumber::plumb("api.R")
-pr$run(host='0.0.0.0', port=as.numeric(Sys.getenv("AIP_HTTP_PORT")))
+predict <- function(req, res) {
+    list(predictions=c(1, 2, 3))
+}
+
+pr() %>%
+    pr_get(Sys.getenv("AIP_HEALTH_ROUTE", "/health"), function() "OK") %>%
+    pr_post(Sys.getenv("AIP_PREDICT_ROUTE", "/predict"), predict) %>%
+    pr_run(host = "0.0.0.0", port=as.integer(Sys.getenv("AIP_HTTP_PORT"), 8080))
