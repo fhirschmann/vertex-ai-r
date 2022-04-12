2ffad62a786784e02632fc2dd03506c4d772b268
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-12 15:33:17 +0200

    fix typo

--- container/serve.R
+++ container/serve.R
@@ -5,10 +5,10 @@ library(plumber)
 #system2("gsutil", c("cp", "-r", Sys.getenv("AIP_STORAGE_URI"), "."))
 
 predict <- function(req, res) {
-    list(predictions=c(1, 2, 3))
+    return(list(predictions=list(1, 2, 3)))
 }
 
 pr() %>%
     pr_get(Sys.getenv("AIP_HEALTH_ROUTE", "/health"), function() "OK") %>%
     pr_post(Sys.getenv("AIP_PREDICT_ROUTE", "/predict"), predict) %>%
-    pr_run(host = "0.0.0.0", port=as.integer(Sys.getenv("AIP_HTTP_PORT"), 8080))
+    pr_run(host = "0.0.0.0", port=as.integer(Sys.getenv("AIP_HTTP_PORT", 8080)))
