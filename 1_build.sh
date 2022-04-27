ac92d6afd481cfe821264f7e9cf61621777cae23
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-27 08:58:58 +0200

    use gcloud builds

--- 1_build.sh
+++ 1_build.sh
@@ -3,5 +3,6 @@ source vars
 
 cd container
 
-docker build -f Dockerfile -t ${IMAGE_URI} ./
-docker push ${IMAGE_URI}
+#docker build -f Dockerfile -t ${IMAGE_URI} ./
+#docker push ${IMAGE_URI}
+gcloud builds submit --region=$LOCATION --tag $IMAGE_URI
