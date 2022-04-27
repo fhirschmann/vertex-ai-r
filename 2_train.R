bacc2de98ef0f634fbffd87f18c902bfcc864688
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-27 08:59:08 +0200

    rename to vertex

--- 2_train.R
+++ 2_train.R
@@ -10,19 +10,19 @@ data_uri <- paste0("gs://", bucket, "/boston.csv")
 library(reticulate)
 use_python(Sys.which("python3"))
 
-aiplatform <- import("google.cloud.aiplatform")
+vertex <- import("google.cloud.aiplatform")
 
-aiplatform$init(project=project_id, location=location, staging_bucket=bucket)
+vertex$init(project=project_id, location=location, staging_bucket=bucket)
 
 write.csv(Boston, "boston.csv", row.names=FALSE)
 system2("gsutil", c("cp", "boston.csv", data_uri))
 
-dataset <- aiplatform$TabularDataset$create(
+dataset <- vertex$TabularDataset$create(
     display_name="Boston Housing Dataset",
     gcs_source=data_uri
 )
 
-job <- aiplatform$CustomContainerTrainingJob(
+job <- vertex$CustomContainerTrainingJob(
     display_name="vertex-r",
     container_uri=image_uri,
     command=c("Rscript", "train.R"),
@@ -34,14 +34,13 @@ model <- job$run(
     dataset=dataset,
     model_display_name="vertex-r-model",
     machine_type="n1-standard-4"
-
 )
 
 print(model$display_name)
 print(model$resource_name)
 print(model$uri)
 
-endpoint <- aiplatform$Endpoint$create(
+endpoint <- vertex$Endpoint$create(
     display_name="Boston Housing Endpoint",
     project=project_id,
     location=location
