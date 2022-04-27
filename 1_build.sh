f5a1d9cae8beb922c09bd395806ff1324d8b5234
Fabian Hirschmann <fabian@hirschmann.email>
2022-04-27 14:43:01 +0200

    base on GCP containers

--- 1_build.sh
+++ 1_build.sh
@@ -5,4 +5,4 @@ cd container
 
 #docker build -f Dockerfile -t ${IMAGE_URI} ./
 #docker push ${IMAGE_URI}
-gcloud builds submit --region=$LOCATION --tag $IMAGE_URI
+gcloud builds submit --region=$LOCATION --tag $IMAGE_URI --timeout=1h
